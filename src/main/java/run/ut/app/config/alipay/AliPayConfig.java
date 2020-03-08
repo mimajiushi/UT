f8a1e3fc7261a817adfe26653de5cb084c98dedc
wenjie <1498780478@qq.com>
2020-03-08 21:32:19 +0800

    增加支付宝相关配置

---
+++ src/main/java/run/ut/app/config/alipay/AliPayConfig.java
@@ -0,0 +1,31 @@
+package run.ut.app.config.alipay;
+
+import com.alipay.api.AlipayClient;
+import com.alipay.api.DefaultAlipayClient;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wenjie
+ */
+
+@Component
+@ConfigurationProperties(prefix = "alipay")
+@Data
+public class AliPayConfig {
+    private String uid;
+    private String appId;
+    private String signType;
+    private String gatewayUrl;
+    private String merchantPrivateKey;
+    private String alipayPublicKey;
+    private String notifyUrl;
+    private String returnUrl;
+
+    @Bean
+    public AlipayClient alipayClient() {
+        return new DefaultAlipayClient(gatewayUrl, appId, merchantPrivateKey, "json", "utf-8", alipayPublicKey, signType);
+    }
+}
