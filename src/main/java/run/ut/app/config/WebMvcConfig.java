5d73a56c38b98d7540500d8fa120c857d55cb139
wenjie <1498780478@qq.com>
2020-04-05 18:24:08 +0800

    修复了无法访问static目录静态资源的问题，引入了freemarker

--- src/main/java/run/ut/app/config/WebMvcConfig.java
+++ src/main/java/run/ut/app/config/WebMvcConfig.java
@@ -5,16 +5,16 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 /**
- * @author swj
+ * @author wenjie
  */
 @Configuration
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Configure swagger's resources
+        // Configure swagger's resources and static resources
         registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/META-INF/resources/");
+                .addResourceLocations("classpath:/META-INF/resources/", "classpath:/static/");
         super.addResourceHandlers(registry);
     }
 }
