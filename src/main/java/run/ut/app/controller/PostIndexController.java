e1dcc6f8c45a4f5b47f950665b40f6d5ea09b7ea
wenjie <1498780478@qq.com>
2020-05-27 12:05:31 +0800

    去掉无用斜杠

--- src/main/java/run/ut/app/controller/PostIndexController.java
+++ src/main/java/run/ut/app/controller/PostIndexController.java
@@ -21,7 +21,7 @@ public class PostIndexController extends BaseController implements PostIndexCont
     private final PostsService postsService;
 
     @Override
-    @GetMapping("/list/post/")
+    @GetMapping("/list/post")
     public CommentPage<PostVO> listPosts(SearchPostParam searchPostParam,
                                          @RequestParam(defaultValue = "1") Integer pageNum,
                                          @RequestParam(defaultValue = "5") Integer pageSize) {
