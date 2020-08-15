1cac24605c93e677e81aed314fa3c08652357b5b
chenwenjie <1498780478@qq.com>
2020-08-15 20:53:40 +0800

    ⭐：网关转发websocket

--- ut-gateway/src/main/java/run/ut/app/UtGateWayApplication.java
+++ ut-gateway/src/main/java/run/ut/app/UtGateWayApplication.java
@@ -3,7 +3,10 @@ package run.ut.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 /**
  * UT main class
@@ -20,5 +23,14 @@ public class UtGateWayApplication {
     public static void main(String[] args) {
         System.setProperty("spring.config.additional-location", "file:${user.home}/.ut-gateway/");
         CONTEXT = SpringApplication.run(UtGateWayApplication.class, args);
+    }
+
+
+    @Bean
+    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+        return builder.routes()
+            // 获取服务实际的ip和端口
+            .route("ut_chat_service_http", r -> r.path("/ws").uri("ws://localhost:8088/ws"))
+            .build();
     }
 }
---
+++ ut-gateway/src/main/java/run/ut/app/WebsocketHandler.java
@@ -0,0 +1,48 @@
+package run.ut.app;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.util.UriComponentsBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.URI;
+
+import static org.springframework.cloud.gateway.support.ServerWebExchangeUtils.GATEWAY_REQUEST_URL_ATTR;
+
+@Component
+@Slf4j
+public class WebsocketHandler implements GlobalFilter, Ordered {
+    private final static String DEFAULT_FILTER_PATH = "/ws";
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+        String upgrade = exchange.getRequest().getHeaders().getUpgrade();
+        log.debug("Upgrade : {}", upgrade);
+        URI requestUrl = exchange.getRequiredAttribute(GATEWAY_REQUEST_URL_ATTR);
+        log.debug("path: {}", requestUrl.getPath());
+        String scheme = requestUrl.getScheme();
+        if (!"ws".equals(scheme) && !"wss".equals(scheme)) {
+            return chain.filter(exchange);
+        } else if (DEFAULT_FILTER_PATH.equals(requestUrl.getPath())) {
+            String wsScheme = convertWsToHttp(scheme);
+            URI wsRequestUrl = UriComponentsBuilder.fromUri(requestUrl).scheme(wsScheme).build().toUri();
+            exchange.getAttributes().put(GATEWAY_REQUEST_URL_ATTR, wsRequestUrl);
+        }
+        return chain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return Ordered.LOWEST_PRECEDENCE - 2;
+    }
+
+    static String convertWsToHttp(String scheme) {
+        scheme = scheme.toLowerCase();
+        return "ws".equals(scheme) ? "http" : "wss".equals(scheme) ? "https" : scheme;
+    }
+}
