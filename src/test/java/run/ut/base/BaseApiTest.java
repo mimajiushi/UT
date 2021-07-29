1ec75f31dba7e1b8503adeb2e650686188065e04
chenwenjie.star <chenwenjie.star@bytedance.com>
2021-07-29 19:51:43 +0800

    🐛: 修复mock日志中文乱码问题

--- src/test/java/run/ut/base/BaseApiTest.java
+++ src/test/java/run/ut/base/BaseApiTest.java
@@ -8,12 +8,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 import org.testng.annotations.BeforeTest;
 import run.ut.app.UtApplication;
 import run.ut.app.model.dto.UserDTO;
@@ -83,19 +81,18 @@ public class BaseApiTest extends AbstractTestNGSpringContextTests {
         emailLoginParam.setEmail(email);
         emailLoginParam.setCode(code);
 
-        ResultActions resultActions = mvc.perform(
+        MvcResult mvcResult = mvc.perform(
                 post(LOGIN_PATH)
                         .content(JsonUtils.objectToJson(emailLoginParam))
-                        .contentType(MediaType.APPLICATION_JSON)
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .accept(MediaType.APPLICATION_JSON_UTF8)
         )
+                .andDo(print())
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$.data", notNullValue()));
-        MockHttpServletResponse response = resultActions.andReturn().getResponse();
-        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
-        resultActions.andDo(print());
-
-        String responseJson = response.getContentAsString(StandardCharsets.UTF_8);
-        return JsonUtils.mapToObject(JsonPath.read(responseJson, "$.data"), UserDTO.class);
+                .andExpect(jsonPath("$.data", notNullValue()))
+                .andReturn();
+        String response = mvcResult.getResponse().getContentAsString(StandardCharsets.UTF_8);
+        return JsonUtils.mapToObject(JsonPath.read(response, "$.data"), UserDTO.class);
     }
 
     /**
@@ -115,4 +112,12 @@ public class BaseApiTest extends AbstractTestNGSpringContextTests {
     public String getAdminToken() throws Exception {
         return loginByAdmin().getToken().getAccessToken();
     }
+
+
+
+
+
+
+
+
 }
